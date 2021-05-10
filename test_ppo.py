937036aae4ecd6e700f54052f7a885821a5a3fdf
smilu97 <smilup2244@gmail.com>
2021-05-10 21:30:23 +0900

    Add custom ppo learner

--- test_ppo.py
+++ test_ppo.py
@@ -1,210 +1,307 @@
 #!/usr/bin/env python
 
+import os
+import glob
+import time
+from datetime import datetime
+
+import torch
 import numpy as np
-import tensorflow as tf 
+
 import gym
-import tensorflow_probability as tfp
-import tensorflow.keras.losses as kls
+
+# import pybullet_envs
+
+from PPO import PPO
+
+# import custom env
 
 from linear_env import Op3LinearEnvironment
 
-launchfile = './op3.launch'
 
-env= Op3LinearEnvironment(launchfile)
-low = env.observation_space.low
-high = env.observation_space.high
+################################### Training ###################################
 
-class critic(tf.keras.Model):
-  def __init__(self):
-    super().__init__()
-    self.d1 = tf.keras.layers.Dense(128,activation='relu')
-    self.v = tf.keras.layers.Dense(1, activation = None)
+def train():
 
-  def call(self, input_data):
-    x = self.d1(input_data)
-    v = self.v(x)
-    return v
+    print("============================================================================================")
+
+
+    ####### initialize environment hyperparameters ######
+
+    env_name = "RobotisOp3-v0"
+
+    has_continuous_action_space = True  # continuous action space; else discrete
+
+    max_ep_len = 5000                   # max timesteps in one episode
+    max_training_timesteps = int(3e6)   # break training loop if timeteps > max_training_timesteps
+
+    print_freq = max_ep_len * 10        # print avg reward in the interval (in num timesteps)
+    log_freq = max_ep_len * 2           # log avg reward in the interval (in num timesteps)
+    save_model_freq = int(1e5)          # save model frequency (in num timesteps)
+
+    action_std = 0.6                    # starting std for action distribution (Multivariate Normal)
+    action_std_decay_rate = 0.05        # linearly decay action_std (action_std = action_std - action_std_decay_rate)
+    min_action_std = 0.1                # minimum action_std (stop decay after action_std <= min_action_std)
+    action_std_decay_freq = int(2.5e5)  # action_std decay frequency (in num timesteps)
+
+    #####################################################
+
+
+    ## Note : print/log frequencies should be > than max_ep_len
+
+
+    ################ PPO hyperparameters ################
+
+    update_timestep = max_ep_len * 4      # update policy every n timesteps
+    K_epochs = 80               # update policy for K epochs in one PPO update
+
+    eps_clip = 0.2          # clip parameter for PPO
+    gamma = 0.99            # discount factor
+
+    lr_actor = 0.0003       # learning rate for actor network
+    lr_critic = 0.001       # learning rate for critic network
+
+    random_seed = 0         # set random seed if required (0 = no random seed)
+
+    #####################################################
+
+
+
+    print("training environment name : " + env_name)
+
+    env = Op3LinearEnvironment('./op3.launch')
+
+    # state space dimension
+    state_dim = env.observation_space.shape[0]
+
+    # action space dimension
+    if has_continuous_action_space:
+        action_dim = env.action_space.shape[0]
+    else:
+        action_dim = env.action_space.n
+
+
+
+    ###################### logging ######################
+
+    #### log files for multiple runs are NOT overwritten
+
+    log_dir = "PPO_logs"
+    if not os.path.exists(log_dir):
+          os.makedirs(log_dir)
+
+    log_dir = log_dir + '/' + env_name + '/'
+    if not os.path.exists(log_dir):
+          os.makedirs(log_dir)
+
+
+    #### get number of log files in log directory
+    run_num = 0
+    current_num_files = next(os.walk(log_dir))[2]
+    run_num = len(current_num_files)
+
+
+    #### create new log file for each run
+    log_f_name = log_dir + '/PPO_' + env_name + "_log_" + str(run_num) + ".csv"
+
+    print("current logging run number for " + env_name + " : ", run_num)
+    print("logging at : " + log_f_name)
+
+    #####################################################
+
+
+    ################### checkpointing ###################
+
+    run_num_pretrained = 0      #### change this to prevent overwriting weights in same env_name folder
+
+    directory = "PPO_preTrained"
+    if not os.path.exists(directory):
+          os.makedirs(directory)
+
+    directory = directory + '/' + env_name + '/'
+    if not os.path.exists(directory):
+          os.makedirs(directory)
+
+
+    checkpoint_path = directory + "PPO_{}_{}_{}.pth".format(env_name, random_seed, run_num_pretrained)
+    print("save checkpoint path : " + checkpoint_path)
+
+    #####################################################
+
+
+    ############# print all hyperparameters #############
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("max training timesteps : ", max_training_timesteps)
+    print("max timesteps per episode : ", max_ep_len)
+
+    print("model saving frequency : " + str(save_model_freq) + " timesteps")
+    print("log frequency : " + str(log_freq) + " timesteps")
+    print("printing average reward over episodes in last : " + str(print_freq) + " timesteps")
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("state space dimension : ", state_dim)
+    print("action space dimension : ", action_dim)
+
+    print("--------------------------------------------------------------------------------------------")
+
+    if has_continuous_action_space:
+        print("Initializing a continuous action space policy")
+        print("--------------------------------------------------------------------------------------------")
+        print("starting std of action distribution : ", action_std)
+        print("decay rate of std of action distribution : ", action_std_decay_rate)
+        print("minimum std of action distribution : ", min_action_std)
+        print("decay frequency of std of action distribution : " + str(action_std_decay_freq) + " timesteps")
+
+    else:
+        print("Initializing a discrete action space policy")
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("PPO update frequency : " + str(update_timestep) + " timesteps")
+    print("PPO K epochs : ", K_epochs)
+    print("PPO epsilon clip : ", eps_clip)
+    print("discount factor (gamma) : ", gamma)
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("optimizer learning rate actor : ", lr_actor)
+    print("optimizer learning rate critic : ", lr_critic)
+
+    if random_seed:
+        print("--------------------------------------------------------------------------------------------")
+        print("setting random seed to ", random_seed)
+        torch.manual_seed(random_seed)
+        env.seed(random_seed)
+        np.random.seed(random_seed)
+
+    #####################################################
+
+    print("============================================================================================")
+
+    ################# training procedure ################
+
+    # initialize a PPO agent
+    ppo_agent = PPO(state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, has_continuous_action_space, action_std)
+
+
+    # track total training time
+    start_time = datetime.now().replace(microsecond=0)
+    print("Started training at (GMT) : ", start_time)
+
+    print("============================================================================================")
+
+
+    # logging file
+    log_f = open(log_f_name,"w+")
+    log_f.write('episode,timestep,reward\n')
+
+
+    # printing and logging variables
+    print_running_reward = 0
+    print_running_episodes = 0
+
+    log_running_reward = 0
+    log_running_episodes = 0
+
+    time_step = 0
+    i_episode = 0
+
+
+    # training loop
+    while time_step <= max_training_timesteps:
+
+        state = env.reset()
+        current_ep_reward = 0
+
+        for t in range(1, max_ep_len+1):
+
+            # select action with policy
+            action = ppo_agent.select_action(state)
+            state, reward, done, _ = env.step(action)
+
+            # saving reward and is_terminals
+            ppo_agent.buffer.rewards.append(reward)
+            ppo_agent.buffer.is_terminals.append(done)
+
+            time_step +=1
+            current_ep_reward += reward
+
+            # update PPO agent
+            if time_step % update_timestep == 0:
+                ppo_agent.update()
+
+            # if continuous action space; then decay action std of ouput action distribution
+            if has_continuous_action_space and time_step % action_std_decay_freq == 0:
+                ppo_agent.decay_action_std(action_std_decay_rate, min_action_std)
+
+            # log in logging file
+            if time_step % log_freq == 0:
+
+                # log average reward till last episode
+                log_avg_reward = log_running_reward / log_running_episodes
+                log_avg_reward = round(log_avg_reward, 4)
+
+                log_f.write('{},{},{}\n'.format(i_episode, time_step, log_avg_reward))
+                log_f.flush()
+
+                log_running_reward = 0
+                log_running_episodes = 0
+
+            # printing average reward
+            if time_step % print_freq == 0:
+
+                # print average reward till last episode
+                print_avg_reward = print_running_reward / print_running_episodes
+                print_avg_reward = round(print_avg_reward, 2)
+
+                print("Episode : {} \t\t Timestep : {} \t\t Average Reward : {}".format(i_episode, time_step, print_avg_reward))
+
+                print_running_reward = 0
+                print_running_episodes = 0
+
+            # save model weights
+            if time_step % save_model_freq == 0:
+                print("--------------------------------------------------------------------------------------------")
+                print("saving model at : " + checkpoint_path)
+                ppo_agent.save(checkpoint_path)
+                print("model saved")
+                print("Elapsed Time  : ", datetime.now().replace(microsecond=0) - start_time)
+                print("--------------------------------------------------------------------------------------------")
+
+            # break; if the episode is over
+            if done:
+                break
+
+        print_running_reward += current_ep_reward
+        print_running_episodes += 1
+
+        log_running_reward += current_ep_reward
+        log_running_episodes += 1
+
+        i_episode += 1
+
+
+    log_f.close()
+    env.close()
+
+
+
+
+    # print total training time
+    print("============================================================================================")
+    end_time = datetime.now().replace(microsecond=0)
+    print("Started training at (GMT) : ", start_time)
+    print("Finished training at (GMT) : ", end_time)
+    print("Total training time  : ", end_time - start_time)
+    print("============================================================================================")
+
+
+
+
+if __name__ == '__main__':
+
+    train()
     
-
-class actor(tf.keras.Model):
-  def __init__(self):
-    super().__init__()
-    self.d1 = tf.keras.layers.Dense(128,activation='relu')
-    self.a = tf.keras.layers.Dense(2,activation='softmax')
-
-  def call(self, input_data):
-    x = self.d1(input_data)
-    a = self.a(x)
-    return a
-
-class agent():
-    def __init__(self, gamma = 0.99):
-        self.gamma = gamma
-        # self.a_opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
-        # self.c_opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
-        self.a_opt = tf.keras.optimizers.Adam(learning_rate=7e-3)
-        self.c_opt = tf.keras.optimizers.Adam(learning_rate=7e-3)
-        self.actor = actor()
-        self.critic = critic()
-        self.clip_pram = 0.2
-
-          
-    def act(self,state):
-        prob = self.actor(np.array([state]))
-        prob = prob.numpy()
-        dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
-        action = dist.sample()
-        return int(action.numpy()[0])
-  
-
-
-    def actor_loss(self, probs, actions, adv, old_probs, closs):
-        
-        probability = probs      
-        entropy = tf.reduce_mean(tf.math.negative(tf.math.multiply(probability,tf.math.log(probability))))
-        #print(probability)
-        #print(entropy)
-        sur1 = []
-        sur2 = []
-        
-        for pb, t, op in zip(probability, adv, old_probs):
-                        t =  tf.constant(t)
-                        op =  tf.constant(op)
-                        #print(f"t{t}")
-                        #ratio = tf.math.exp(tf.math.log(pb + 1e-10) - tf.math.log(op + 1e-10))
-                        ratio = tf.math.divide(pb,op)
-                        #print(f"ratio{ratio}")
-                        s1 = tf.math.multiply(ratio,t)
-                        #print(f"s1{s1}")
-                        s2 =  tf.math.multiply(tf.clip_by_value(ratio, 1.0 - self.clip_pram, 1.0 + self.clip_pram),t)
-                        #print(f"s2{s2}")
-                        sur1.append(s1)
-                        sur2.append(s2)
-
-        sr1 = tf.stack(sur1)
-        sr2 = tf.stack(sur2)
-        
-        #closs = tf.reduce_mean(tf.math.square(td))
-        loss = tf.math.negative(tf.reduce_mean(tf.math.minimum(sr1, sr2)) - closs + 0.001 * entropy)
-        #print(loss)
-        return loss
-
-    def learn(self, states, actions,  adv , old_probs, discnt_rewards):
-        discnt_rewards = tf.reshape(discnt_rewards, (len(discnt_rewards),))
-        adv = tf.reshape(adv, (len(adv),))
-
-        old_p = old_probs
-
-        old_p = tf.reshape(old_p, (len(old_p),2))
-        with tf.GradientTape() as tape1, tf.GradientTape() as tape2:
-            p = self.actor(states, training=True)
-            v =  self.critic(states,training=True)
-            v = tf.reshape(v, (len(v),))
-            td = tf.math.subtract(discnt_rewards, v)
-            c_loss = 0.5 * kls.mean_squared_error(discnt_rewards, v)
-            a_loss = self.actor_loss(p, actions, adv, old_probs, c_loss)
-            
-        grads1 = tape1.gradient(a_loss, self.actor.trainable_variables)
-        grads2 = tape2.gradient(c_loss, self.critic.trainable_variables)
-        self.a_opt.apply_gradients(zip(grads1, self.actor.trainable_variables))
-        self.c_opt.apply_gradients(zip(grads2, self.critic.trainable_variables))
-        return a_loss, c_loss
-
-def test_reward(env):
-  total_reward = 0
-  state = env.reset()
-  done = False
-  while not done:
-    action = np.argmax(agentoo7.actor(np.array([state])).numpy())
-    next_state, reward, done, _ = env.step(action)
-    state = next_state
-    total_reward += reward
-
-  return total_reward
-
-def preprocess1(states, actions, rewards, done, values, gamma):
-    g = 0
-    lmbda = 0.95
-    returns = []
-    for i in reversed(range(len(rewards))):
-       delta = rewards[i] + gamma * values[i + 1] * done[i] - values[i]
-       g = delta + gamma * lmbda * dones[i] * g
-       returns.append(g + values[i])
-
-    returns.reverse()
-    adv = np.array(returns, dtype=np.float32) - values[:-1]
-    adv = (adv - np.mean(adv)) / (np.std(adv) + 1e-10)
-    states = np.array(states, dtype=np.float32)
-    actions = np.array(actions, dtype=np.int32)
-    returns = np.array(returns, dtype=np.float32)
-    return states, actions, returns, adv    
-
-
-tf.random.set_seed(336699)
-agentoo7 = agent()
-steps = 5000
-ep_reward = []
-total_avgr = []
-target = False 
-best_reward = 0
-avg_rewards_list = []
-
-
-for s in range(steps):
-  if target == True:
-          break
-  
-  done = False
-  state = env.reset()
-  all_aloss = []
-  all_closs = []
-  rewards = []
-  states = []
-  actions = []
-  probs = []
-  dones = []
-  values = []
-  print("new episod")
-
-  for e in range(128):
-   
-    action = agentoo7.act(state)
-    value = agentoo7.critic(np.array([state])).numpy()
-    next_state, reward, done, _ = env.step(action)
-    dones.append(1-done)
-    rewards.append(reward)
-    states.append(state)
-    #actions.append(tf.one_hot(action, 2, dtype=tf.int32).numpy().tolist())
-    actions.append(action)
-    prob = agentoo7.actor(np.array([state]))
-    probs.append(prob[0])
-    values.append(value[0][0])
-    state = next_state
-    if done:
-      env.reset()
-  
-  value = agentoo7.critic(np.array([state])).numpy()
-  values.append(value[0][0])
-  np.reshape(probs, (len(probs),2))
-  probs = np.stack(probs, axis=0)
-
-  states, actions,returns, adv  = preprocess1(states, actions, rewards, dones, values, 1)
-
-  for epocs in range(10):
-      al,cl = agentoo7.learn(states, actions, adv, probs, returns)
-      # print(f"al{al}") 
-      # print(f"cl{cl}")   
-
-  avg_reward = np.mean([test_reward(env) for _ in range(5)])
-  print(f"total test reward is {avg_reward}")
-  avg_rewards_list.append(avg_reward)
-  if avg_reward > best_reward:
-        print('best reward=' + str(avg_reward))
-        agentoo7.actor.save('model_actor_{}_{}'.format(s, avg_reward), save_format="tf")
-        agentoo7.critic.save('model_critic_{}_{}'.format(s, avg_reward), save_format="tf")
-        best_reward = avg_reward
-  if best_reward == 200:
-        target = True
-  env.reset()
-
-env.close()
