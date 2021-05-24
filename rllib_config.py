aac4c958d3b5dc0a62ca82f806ceff2bd9df0a50
smilu97 <smilup2244@gmail.com>
2021-05-24 17:12:21 +0900

    More gradient descent

--- rllib_config.py
+++ rllib_config.py
@@ -8,8 +8,8 @@ config = {
     "kl_coeff": 1.0,
     "num_sgd_iter": 20,
     "lr": 0.0001,
-    "sgd_minibatch_size": 10000,
-    "train_batch_size": 10000,
+    "sgd_minibatch_size": 1000,
+    "train_batch_size": 5000,
     "model": {
         "free_log_std": True
     },
