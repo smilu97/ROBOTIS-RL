a49e986a686ef0d954c4251467f768bc062afdfc
smilu97 <smilup2244@gmail.com>
2021-07-15 14:41:04 +0900

    increase train batch size

--- ppo_config.py
+++ ppo_config.py
@@ -1,5 +1,5 @@
 config = {
-    "num_workers": 8,
+    "num_workers": 6,
     "num_envs_per_worker": 1,
     "num_gpus": 1,
     "gamma": 0.995,
@@ -8,11 +8,12 @@ config = {
     "kl_coeff": 1.0,
     "num_sgd_iter": 30,
     "lr": 0.0003,
-    "sgd_minibatch_size": 2048,
-    "train_batch_size": 8192,
+    "sgd_minibatch_size": 6144,
+    "train_batch_size": 65536,
     "model": {
         "free_log_std": True
     },
+    "vf_clip_param": 100.0,
     "batch_mode": "complete_episodes",
     "observation_filter": "MeanStdFilter",
     "model": {
