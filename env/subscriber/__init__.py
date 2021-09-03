74b7ac10892ca59f9d13a899cf59911c92dd4852
smilu97 <smilup2244@gmail.com>
2021-09-03 11:47:28 +0900

    add .wait method in subscriber

--- env/subscriber/__init__.py
+++ env/subscriber/__init__.py
@@ -1,17 +1,28 @@
 import rospy
+import threading
 
 class RosSubscriber:
     def __init__(self, path, state_type, queue_size = 10):
         self.updated = False
         self.latest = None
+        self.wakeup = False
+        self.evt_wakeup = threading.Event()
 
         def callback(data):
             self.updated = True
             self.latest = data
+            self.wakeup = True
+            self.evt_wakeup.set()
 
         self.subscriber = \
             rospy.Subscriber(path, state_type, callback, queue_size=queue_size)
     
+    def wait(self):
+        if self.wakeup: return
+        self.evt_wakeup.wait(0.1)
+        self.wakeup = False
+        self.evt_wakeup.clear()
+
     def reset(self):
         self.updated = False
         self.latest = None
