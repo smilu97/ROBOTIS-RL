c6e732a0bf0c3dd5d4a8d8fb4b43b4d11013869c
smilu97 <smilup2244@gmail.com>
2021-09-03 11:41:47 +0900

    ppo_config checkpoint

--- ppo_config.py
+++ ppo_config.py
@@ -1,26 +1,26 @@
 config = {
-    "num_workers": 10,
+    "num_workers": 8,
     "num_envs_per_worker": 1,
     "num_gpus": 1,
-    "gamma": 0.995,
+    "gamma": 0.99,
     "lambda": 0.95,
-    "clip_param": 0.2,
+    "clip_param": 0.3,
     "kl_coeff": 1.0,
     "num_sgd_iter": 30,
     "lr": 0.0003,
-    "sgd_minibatch_size": 256,
-    "train_batch_size": 10000,
+    "sgd_minibatch_size": 128,
+    "train_batch_size": 4000,
     "model": {
         "free_log_std": True
     },
     "entropy_coeff": 0.0001,
     "explore": True,
     "vf_clip_param": 100.0,
-    "batch_mode": "complete_episodes",
+    "batch_mode": "truncate_episodes",
     "observation_filter": "MeanStdFilter",
     "model": {
-        "fcnet_hiddens": [180, 125],
-        "fcnet_activation": "tanh",
+        "fcnet_hiddens": [128, 86],
+        "fcnet_activation": "relu",
         "use_lstm": False
     },
 }
